ee6a730d956bbf75e1ea82dd2f4e7a5d4b592499
isak-mps <isakmarais4@gmail.com>
2024-08-21 22:23:32 +0200

    new file:   assets/images/default.jpg     new file:   components/Footer/index.tsx 	new file:   components/Hero/index.tsx 	new file:   components/Layout/index.tsx 	new file:   components/Nav/index.tsx 	new file:   components/PostBlock/index.tsx 	new file:   git 	new file:   lib/base.ts 	new file:   lib/service.ts 	deleted:    my-app 	new file:   next.config.js 	new file:   package-lock.json 	new file:   package.json 	new file:   pages/_app.tsx 	new file:   pages/_document.tsx 	new file:   pages/api/hello.ts 	new file:   pages/index.tsx 	new file:   pages/posts/[slug].tsx 	new file:   postcss.config.js 	new file:   public/favicon.ico 	new file:   public/next.svg 	new file:   public/thirteen.svg 	new file:   public/vercel.svg 	new file:   styles/globals.css 	new file:   tailwind.config.js 	new file:   tsconfig.json

---
+++ next.config.js
@@ -0,0 +1,10 @@
+ /** @type {import('next').NextConfig} */
+ const nextConfig = {
+   reactStrictMode: true,
+   images: {
+     domains: ["mps-apps.local", "mps-apps.com"],
+   },
+ };
+
+ module.exports = nextConfig;
+
